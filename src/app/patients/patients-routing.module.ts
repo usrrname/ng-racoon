47c07a7eb33b1de2b0b10da4477d3177a1545758
Jennifer Chan <jchan@Jennifers-MacBook-Pro.local>
2019-12-25 13:23:59 -0500

    lazy load create-patient path

--- src/app/patients/patients-routing.module.ts
+++ src/app/patients/patients-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PatientsComponent } from './patients/patients.component';
 import { HomeLayoutComponent } from '../shared/components/layouts/home-layout/home-layout.component';
+import { CreatePatientComponent } from './create-patient/create-patient.component';
 
 const routes: Routes = [
   {
-   path: '', component: HomeLayoutComponent, children: [
-     { path: '', component: PatientsComponent, pathMatch: 'full' }
+   path: '', component: HomeLayoutComponent, 
+   children: [
+     { path: '', component: PatientsComponent, pathMatch: 'full' },
+     { path: 'create-patient', component: CreatePatientComponent, pathMatch: 'full'}
    ]
   }
 ];
